42191dc95ee748e3cdeae543962397ef709dc159
Emmanuella-codes <81877624+Emmanuella-codes@users.noreply.github.com>
2022-12-09 17:17:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# codewars-8kyu-training-on-exclusive-or-xor-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-bafftm)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Write Javascript code!
+function xor(a, b) {
+  // TODO: Program Me
+  return (a && !b) || (!a && b)
+}
+
+console.log(xor(false, false))
+console.log(xor(false, true))
+console.log(xor(true, true))
+console.log(xor(true, false))
+
+
